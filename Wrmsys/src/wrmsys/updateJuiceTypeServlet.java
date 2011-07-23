8fe87d0514521296990eeed4800df8600004375c
4es3 <4es3@192.168.244.139>
2011-07-23 01:20:42 -0400

    Commit da shit.

---
+++ Wrmsys/src/wrmsys/updateJuiceTypeServlet.java
@@ -0,0 +1,12 @@
+package wrmsys;
+
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import javax.servlet.http.*;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+public class updateJuiceTypeServlet extends HttpServlet {
+
+}
