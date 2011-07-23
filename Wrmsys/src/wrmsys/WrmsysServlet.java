8daac9d4ad4999aa8c56999bab05cf6133859d71
4es3 <4es3@192.168.244.139>
2011-07-23 00:19:19 -0400

    ack

--- Wrmsys/src/wrmsys/WrmsysServlet.java
+++ Wrmsys/src/wrmsys/WrmsysServlet.java
@@ -10,17 +10,14 @@ import com.google.appengine.api.users.UserServiceFactory;
 public class WrmsysServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		UserService userService = UserServiceFactory.getUserService();
-		User user = userService.getCurrentUser();
-		if (user != null) {
-			resp.setContentType("text/html");
-			resp.getWriter().println(
-					"<html><body>Hello " + user.getNickname()
-							+ "! Welcome to WRMS main page! You can <a href='"
-							+ userService.createLogoutURL(req.getRequestURI())
-							+ "'>sign out</a></body></html>");
-		} else {
-			resp.sendRedirect(userService.createLoginURL(req.getRequestURI()));
-		}
+	    UserService userService = UserServiceFactory.getUserService();
+	    User user = userService.getCurrentUser();
+	    if (user != null) {
+	    	resp.setContentType("text/html");
+	    	resp.getWriter().println("<html><body>Hello " + user.getNickname() + "! Welcome to WRMS main page! You can <a href='"+ userService.createLogoutURL(req.getRequestURI()) +"'>sign out</a></body></html>");
+	    }
+	    else {
+	    	resp.sendRedirect(userService.createLoginURL(req.getRequestURI()));
+	    }
 	}
 }
